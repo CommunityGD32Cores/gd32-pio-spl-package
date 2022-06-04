543c6bc452265b4a71afcddde92be5b6a58121c2
unknown <maximilian.gerhardt@rub.de>
2022-06-04 23:41:24 +0200

    Fix F3x0 bool type

--- gd32/cmsis/variants/gd32f3x0/gd32f3x0.h
+++ gd32/cmsis/variants/gd32f3x0/gd32f3x0.h
@@ -172,10 +172,11 @@ extern "C" {
 #include "core_cm4.h"
 #include "system_gd32f3x0.h"
 #include <stdint.h>
+#include <stdbool.h>
 
     /* enum definitions */
     typedef enum {DISABLE = 0, ENABLE = !DISABLE} EventStatus, ControlStatus;
-    typedef enum {FALSE = 0, TRUE = !FALSE} bool;
+    //typedef enum {FALSE = 0, TRUE = !FALSE} bool;
     typedef enum {RESET = 0, SET = !RESET} FlagStatus;
     typedef enum {ERROR = 0, SUCCESS = !ERROR} ErrStatus;
 
